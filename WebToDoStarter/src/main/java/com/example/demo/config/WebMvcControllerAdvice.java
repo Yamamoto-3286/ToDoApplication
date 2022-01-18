d9cdbf8549cb9dc98423ea8f70edb173b3e8e635
yamamoto <5mxSkPtB@gmail.com>
2022-01-18 23:40:24 +0900

    Spring入門 chapter56~60

--- WebToDoStarter/src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ WebToDoStarter/src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -9,20 +9,23 @@ import org.springframework.web.bind.annotation.InitBinder;
 
 import com.example.demo.service.TaskNotFoundException;
 
-
 @ControllerAdvice
-public class WebMvcControllerAdvice {
+public class WebMvcControllerAdvice
+{
 
-    @InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
-        // 入力値の空文字をnullに変換
-        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-    }
-    
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder)
+	{
+		// 入力値の空文字をnullに変換
+		dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+
+	// TaskNotFoundExceptionが飛んできたときに例外を送出
 	@ExceptionHandler(TaskNotFoundException.class)
-	public String handleException(TaskNotFoundException e,Model model) {
+	public String handleException(TaskNotFoundException e, Model model)
+	{
 		model.addAttribute("message", e);
 		return "error/CustomPage";
 	}
-    
+
 }
